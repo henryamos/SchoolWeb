a6114a8ade18fd40d88a0db9f1878f58a6b3a793
henry amos <amoshenry95@gmail.com>
2021-08-10 18:35:14 +0000

    Edisec FrontEnd

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Toogle Menu */
+
+let navLinks = document.getElementById("navLinks");
+
+function displayMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
